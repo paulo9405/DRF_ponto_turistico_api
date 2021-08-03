b63bae9cce7a9f905e91b07f6d37798c67dec98c
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-08-03 16:08:02 +0100

    config a app

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turisticos94.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['pontos-turisticos94.herokuapp.com', 'localhost']
 
 
 INSTALLED_APPS = [
